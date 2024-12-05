fcde2bbf03789db70eb167685c236fc11b184e84
jstanton11 <146177489+jstanton11@users.noreply.github.com>
2024-12-05 10:27:58 +0000

    web y controllernotes

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
