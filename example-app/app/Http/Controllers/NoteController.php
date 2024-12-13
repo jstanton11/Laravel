55d0932996f33c404f97c46758d11c54b2676eeb
jstanton11 <146177489+jstanton11@users.noreply.github.com>
2024-12-13 10:16:21 +0000

    ogkoakg

--- example-app/app/Http/Controllers/NoteController.php
+++ example-app/app/Http/Controllers/NoteController.php
@@ -3,45 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Note;
+use Illuminate\View\View;
 
 class NoteController extends Controller
 {
-    public function index(){
+    public function index(): View
+    {
         $notes = Note::all();
         return view('note.index', compact('notes'));
     }
 
-    public function form(){
+    public function form(): View
+    {
         return view('note.formulario');
     }
 
-    public function store(Request $request){
+    public function store(Request $request): RedirectResponse
+    {
         Note::create($request->all());
 
         return redirect()->route('note.index');
     }
 
-    public function edit(Note $note){
+    public function edit(Note $note): View
+    {
         
         return view('note.edit', compact('note'));
 
     }
 
-    public function update(Request $request, Note $note){
+    public function update(Request $request, Note $note): RedirectResponse
+    {
         
         $note->update($request->all());
         return redirect()->route('note.index');
 
     }
 
-    public function show(Note $note){
+    public function show(Note $note): View
+    {
         
         return view('note.show', compact('note'));
 
     }
 
-    public function destroy(Note $note){
+    public function destroy(Note $note): RedirectResponse
+    {
         
         $note->delete();
         return redirect()->route('note.index');
